e054c57532542af3723a9a0669bac24f71e8e651
Ron Cooke <ron@astonspark.com>
2021-06-15 14:25:24 -0400

    Move tools to utils

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -5,7 +5,7 @@ function M.makeScratch()
   api.nvim_command('enew')
   vim.bo[0].buftype = 'nofile'
   vim.bo[0].bufhidden = 'hide'
-  vim.bo[0].swapfile = false
+  vim.bo[0].buftype = false
 end
 
 return M
