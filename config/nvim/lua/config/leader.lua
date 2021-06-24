2f0e6e41435d2de73f4ef63993101d287a2064b7
Ron Cooke <ron@astonspark.com>
2021-06-24 19:22:22 -0400

    Add leader key mappings

--- config/nvim/lua/config/leader.lua
+++ config/nvim/lua/config/leader.lua
@@ -17,11 +17,13 @@ lmap.b = {
   d = 'Delete Current Buffer',
   n = 'Next Buffer',
   p = 'Previous Buffer',
+  t = 'Tags',
 }
 leader_map('bb', [[:Buffers<cr>]])
 leader_map('bd', [[:bdelete<cr>]])
 leader_map('bn', [[:bnext<cr>]])
 leader_map('bp', [[:bprevious<cr>]])
+leader_map('bt', [[:BTags<cr>]])
 
 -- Environment
 lmap.e = {
@@ -53,7 +55,7 @@ leader_map('ePp', [[:PackerInstall<cr>]])
 leader_map('ePs', [[:PackerSync<cr>]])
 leader_map('ePS', [[:PackerStatus<cr>]])
 leader_map('ePu', [[:PackerUpdate<cr>]])
-leader_map('er', [[:registers<cr>]])
+leader_map('er', [[:Telescope registers<cr>]])
 leader_map('eR', [[:source $MYVIMRC<cr>]])
 leader_map('es', [[:Scratch<cr>]])
 leader_map('et', [[:NERDTreeFocus | vertical resize 31<cr>]])
@@ -62,14 +64,57 @@ leader_map('et', [[:NERDTreeFocus | vertical resize 31<cr>]])
 lmap.f = {
   name = '+File',
   f = 'Find File',
+  r = 'Reload File',
+  R = 'Force Reload File',
   s = 'Save File',
+  S = 'Save All',
+  t = 'Find in File Tree',
+  x = 'Save & Close',
 }
--- lmap.f = {name = 'File'}
--- lmap.f.f = 'Find File'
--- lmap.f.s = 'Save File'
 leader_map('ff', [[:Telescope find_files<cr>]])
+leader_map('fr', [[:e<cr>]])
+leader_map('fR', [[:e!<cr>]])
 leader_map('fs', [[:w<cr>]])
+leader_map('fS', [[:wa<cr>]])
+leader_map('ft', [[:NERDTreeFind<cr>]])
+leader_map('fx', [[:x<cr>]])
 
+-- Project
+lmap.p = {
+  name = '+Project',
+  a = 'Alternate File',
+  f = 'Toggle File Tree',
+  F = 'Focus File Tree',
+  t = 'Tags',
+  T = 'Generate Tags',
+}
+leader_map('pa', [[:A<cr>]])
+leader_map('pf', [[:NERDTreeToggle<cr>]])
+leader_map('pF', [[:NERDTreeFocus<cr>]])
+leader_map('pt', [[:Telescope tags<cr>]])
+leader_map('pT', [[:! ctags<cr>]])
+
+-- Text
+lmap.x = {
+  name = '+Text',
+  n = 'Split New Line',
+  s = 'Substitute',
+  S = 'Line Substitute',
+  y = 'Yank to Clipboard',
+  Y = 'Yank Line to Clipboard',
+}
+cmd [[command! SplitNewline call feedkeys('i<cr><esc>', 't')]]
+cmd [[command! NSubstitute call feedkeys(':s/', 't')]]
+cmd [[command! LSubstitute call feedkeys('V:s/', 't')]]
+cmd [[command! NYankToClip call feedkeys('"*y<esc>', 't')]]
+cmd [[command! LYankToClip call feedkeys('^v$h"*y', 't')]]
+leader_map('xn', [[:SplitNewline<cr>]])
+leader_map('xs', [[:NSubstitute<cr>]])
+leader_map('xS', [[:LSubstitute<cr>]])
+leader_map('xy', [[:NYankToClip<cr>]])
+leader_map('xY', [[:LYankToClip<cr>]])
+
+-- Quit
 lmap.q = {name = 'Quit'}
 lmap.q.q = 'Quit All'
 lmap.q.q = 'Force Quit All'
