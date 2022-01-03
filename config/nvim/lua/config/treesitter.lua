2e1dec468648f50bd1a9df59aa039a60aac570cf
Ron Cooke <ron@astonspark.com>
2022-01-03 16:47:09 -0500

    Fix weird indenting

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.configs").setup {
   ensure_installed = {"ruby", "javascript", "lua"},
   highlight = {enable = true},
-  indent = {enable = true},
+  indent = {enable = false},
   context_commentstring = {enable = true},
   matchup = {enable = true}
 }
