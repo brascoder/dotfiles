d279053f3acfc26d2d7c3445d197ad863b4bc42f
Brasco Cooke <ron@astonspark.com>
2026-04-15 10:23:14 -0400

    Add labels for leader keybindings

--- config/nvim/lua/config/leader.lua
+++ config/nvim/lua/config/leader.lua
@@ -6,99 +6,114 @@ local cmd = vim.cmd
 funcs = require("config.funcs")
 
 -- Top Level
-nleader(" ", [[:Telescope commands<CR>]])
+nleader(" ", [[:Telescope commands<CR>]], "Commands")
 
 -- Buffer
 cmd [[command! LastBuffer call feedkeys("<C-^>", "t")]]
-nleader("bb", [[:Telescope buffers show_all_buffers=true<CR>]])
-nleader("bd", [[:bdelete<CR>]])
-nleader("bl", [[:LastBuffer<CR>]])
-nleader("bm", [[:Telescope marks<CR>]])
-nleader("bn", [[:bnext<CR>]])
-nleader("bo", [[:BufOnly<CR>]])
-nleader("bp", [[:bprevious<CR>]])
-nleader("bs", [[:Telescope current_buffer_fuzzy_find<CR>]])
-nleader("bt", [[:Telescope current_buffer_tags<CR>]])
+nleader("bb", [[:Telescope buffers show_all_buffers=true<CR>]], "List buffers")
+nleader("bd", [[:bdelete<CR>]], "Delete buffer")
+nleader("bl", [[:LastBuffer<CR>]], "Last buffer")
+nleader("bm", [[:Telescope marks<CR>]], "Marks")
+nleader("bn", [[:bnext<CR>]], "Next buffer")
+nleader("bo", [[:BufOnly<CR>]], "Close other buffers")
+nleader("bp", [[:bprevious<CR>]], "Previous buffer")
+nleader("bs", [[:Telescope current_buffer_fuzzy_find<CR>]], "Search buffer")
+nleader("bt", [[:Telescope current_buffer_tags<CR>]], "Buffer tags")
+
+-- Claude Code
+nleader("cc", [[:ClaudeCode<CR>]], "Open Claude")
+nleader("cf", [[:ClaudeCodeFocus<CR>]], "Focus Claude")
+nleader("ca", [[:ClaudeCodeAdd %<CR>]], "Add file to Claude")
+nleader("cC", [[:ClaudeCode --continue<CR>]], "Continue task")
+nleader("cm", [[:ClaudeCodeSelectModel<CR>]], "Select model")
+nleader("cr", [[:ClaudeCode --resume<CR>]], "Resume session")
+nleader("ct", [[:ClaudeCodeTreeAdd<CR>]], "Add file from tree")
+nleader("cda", [[:ClaudeCodeDiffAccept<CR>]], "Accept diff")
+nleader("cdd", [[:ClaudeCodeDiffDeny<CR>]], "Deny diff")
+vleader("cs", [[:ClaudeCodeSend<CR>]], "Send selection to Claude")
 
 -- Diagnostics
-nleader("dl", [[:Telescope diagnostics<CR>]])
+nleader("dl", [[:Telescope diagnostics<CR>]], "List diagnostics")
 
 -- Environment
 cmd [[command! Scratch lua funcs.make_scratch()]]
-nleader("ea", [[:setlocal spell!<CR>]])
-nleader("eh", [[:set hlsearch!<CR>]])
-nleader("ei", [[:IBLToggle<CR>]])
-nleader("em", [[:messages<CR>]])
-nleader("epp", [[:Lazy install<CR>]])
-nleader("eps", [[:Lazy sync<CR>]])
-nleader("epS", [[:Lazy<CR>]])
-nleader("epu", [[:Lazy update<CR>]])
-nleader("er", [[:Telescope registers<CR>]])
-nleader("eR", [[:luafile %<CR>]])
-nleader("es", [[:Scratch<CR>]])
-nleader("ef", [[:lua vim.g.conform_format_on_save = not vim.g.conform_format_on_save; print("Format on save: " .. tostring(vim.g.conform_format_on_save))<CR>]])
-nleader("et", [[:NvimTreeRefresh<CR>]])
+nleader("ea", [[:setlocal spell!<CR>]], "Toggle spell check")
+nleader("eh", [[:set hlsearch!<CR>]], "Toggle search highlight")
+nleader("ei", [[:IBLToggle<CR>]], "Toggle indent guides")
+nleader("em", [[:messages<CR>]], "Show messages")
+nleader("epp", [[:Lazy install<CR>]], "Install plugins")
+nleader("eps", [[:Lazy sync<CR>]], "Sync plugins")
+nleader("epS", [[:Lazy<CR>]], "Plugin manager")
+nleader("epu", [[:Lazy update<CR>]], "Update plugins")
+nleader("er", [[:Telescope registers<CR>]], "Registers")
+nleader("eR", [[:luafile %<CR>]], "Reload current file")
+nleader("es", [[:Scratch<CR>]], "Scratch buffer")
+nleader("ef", [[:lua vim.g.conform_format_on_save = not vim.g.conform_format_on_save; print("Format on save: " .. tostring(vim.g.conform_format_on_save))<CR>]], "Toggle format on save")
+nleader("et", [[:NvimTreeRefresh<CR>]], "Refresh file tree")
 
 -- File
-nleader("fb", [[:Telescope file_browser<CR>]])
-nleader("ff", [[:Telescope find_files<CR>]])
-nleader("fg", [[:Telescope git_files<CR>]])
-nleader("fp", [[:let @*=@%<CR>]])
-nleader("fr", [[:e<CR>]])
-nleader("fR", [[:e!<CR>]])
-nleader("fs", [[:w<CR>]])
-nleader("fS", [[:wa<CR>]])
-nleader("ft", [[:NvimTreeToggle<CR>]])
-nleader("fT", [[:NvimTreeFindFile<CR>]])
-nleader("fx", [[:x<CR>]])
+nleader("fb", [[:Telescope file_browser<CR>]], "Browse files")
+nleader("ff", [[:Telescope find_files<CR>]], "Find file")
+nleader("fg", [[:Telescope git_files<CR>]], "Find git file")
+nleader("fp", [[:let @*=@%<CR>]], "Copy file path")
+nleader("fr", [[:e<CR>]], "Reload file")
+nleader("fR", [[:e!<CR>]], "Force reload file")
+nleader("fs", [[:w<CR>]], "Save file")
+nleader("fS", [[:wa<CR>]], "Save all files")
+nleader("ft", [[:NvimTreeToggle<CR>]], "Toggle file tree")
+nleader("fT", [[:NvimTreeFindFile<CR>]], "Reveal in file tree")
+nleader("fx", [[:x<CR>]], "Save and close")
 
--- QuickFix
-nleader("Fc", [[:cclose<CR>]])
-nleader("Fo", [[:copen<CR>]])
+-- Flutter
+nleader("vr", [[:FlutterRun<CR>]], "Run")
+nleader("vR", [[:FlutterRestart<CR>]], "Restart")
+nleader("vh", [[:FlutterReload<CR>]], "Hot reload")
+nleader("vd", [[:FlutterDevices<CR>]], "Devices")
+nleader("vo", [[:FlutterOutlineToggle<CR>]], "Toggle outline")
 
 -- Git
-nleader("gb", [[:Git blame<CR>]])
-nleader("gB", [[:Telescope git_branches<CR>]])
-nleader("gc", [[:Telescope git_commits<CR>]])
-nleader("gg", [[:LazyGit<CR>]])
-nleader("go", [[:Telescope oldfiles<CR>]])
-nleader("gs", [[:Telescope git_status<CR>]])
-nleader("gS", [[:Telescope git_stash<CR>]])
+nleader("gb", [[:Git blame<CR>]], "Blame")
+nleader("gB", [[:Telescope git_branches<CR>]], "Branches")
+nleader("gc", [[:Telescope git_commits<CR>]], "Commits")
+nleader("gg", [[:LazyGit<CR>]], "LazyGit")
+nleader("go", [[:Telescope oldfiles<CR>]], "Recent files")
+nleader("gs", [[:Telescope git_status<CR>]], "Status")
+nleader("gS", [[:Telescope git_stash<CR>]], "Stash")
 
-nleader("ghb", [[:Gitsigns blame_line<CR>]])
-nleader("ghh", [[:Gitsigns select_hunk<CR>]])
-nleader("ghn", [[:Gitsigns next_hunk<CR>]])
-nleader("ghp", [[:Gitsigns prev_hunk<CR>]])
-nleader("ghP", [[:Gitsigns preview_hunk<CR>]])
-nleader("ghr", [[:Gitsigns reset_hunk<CR>]])
-nleader("ghR", [[:Gitsigns reset_buffer<CR>]])
-nleader("ghs", [[:Gitsigns stage_hunk<CR>]])
-nleader("ghu", [[:Gitsigns undo_stage_hunk<CR>]])
+nleader("ghb", [[:Gitsigns blame_line<CR>]], "Blame line")
+nleader("ghh", [[:Gitsigns select_hunk<CR>]], "Select hunk")
+nleader("ghn", [[:Gitsigns next_hunk<CR>]], "Next hunk")
+nleader("ghp", [[:Gitsigns prev_hunk<CR>]], "Previous hunk")
+nleader("ghP", [[:Gitsigns preview_hunk<CR>]], "Preview hunk")
+nleader("ghr", [[:Gitsigns reset_hunk<CR>]], "Reset hunk")
+nleader("ghR", [[:Gitsigns reset_buffer<CR>]], "Reset buffer")
+nleader("ghs", [[:Gitsigns stage_hunk<CR>]], "Stage hunk")
+nleader("ghu", [[:Gitsigns undo_stage_hunk<CR>]], "Undo stage hunk")
 
 -- Hop
-nleader("hh", [[:HopChar1<CR>]])
-nleader("hl", [[:HopLine<CR>]])
-nleader("hp", [[:HopPattern<CR>]])
+nleader("hh", [[:HopChar1<CR>]], "Hop to char")
+nleader("hl", [[:HopLine<CR>]], "Hop to line")
+nleader("hp", [[:HopPattern<CR>]], "Hop to pattern")
 
 -- Project
-nleader("pa", [[:A<CR>]])
-nleader("ps", [[:Telescope live_grep<CR>]])
-nleader("pS", [[:Telescope grep_string<CR>]])
-nleader("pt", [[:Telescope tags<CR>]])
-nleader("pT", [[:! ctags<CR>]])
+nleader("pa", [[:A<CR>]], "Alternate file")
+nleader("ps", [[:Telescope live_grep<CR>]], "Search project")
+nleader("pS", [[:Telescope grep_string<CR>]], "Search word")
+nleader("pt", [[:Telescope tags<CR>]], "Tags")
+nleader("pT", [[:! ctags<CR>]], "Generate tags")
 
 -- Terminal
-nleader("zo", [[:ToggleTerm direction=horizontal<CR>]])
-nleader("zv", [[:ToggleTerm direction=vertical<CR>]])
-nleader("zf", [[:ToggleTerm direction=float<CR>]])
-nleader("zr", [[:ToggleTermSendCurrentLine<CR>]])
-vleader("zr", [[:ToggleTermSendVisualSelection<CR>]])
+nleader("zo", [[:ToggleTerm direction=horizontal<CR>]], "Horizontal terminal")
+nleader("zv", [[:ToggleTerm direction=vertical<CR>]], "Vertical terminal")
+nleader("zf", [[:ToggleTerm direction=float<CR>]], "Floating terminal")
+nleader("zr", [[:ToggleTermSendCurrentLine<CR>]], "Send line to terminal")
+vleader("zr", [[:ToggleTermSendVisualSelection<CR>]], "Send selection to terminal")
 
 -- Test
-nleader("tl", [[:TestLast<CR>]])
-nleader("tn", [[:TestNearest<CR>]])
-nleader("ts", [[:TestSuite<CR>]])
-nleader("tt", [[:TestFile<CR>]])
+nleader("tl", [[:TestLast<CR>]], "Run last test")
+nleader("tn", [[:TestNearest<CR>]], "Run nearest test")
+nleader("ts", [[:TestSuite<CR>]], "Run test suite")
+nleader("tt", [[:TestFile<CR>]], "Run test file")
 
 -- Text
 cmd [[command! InsertAbove call feedkeys("O<Esc>j", "t")]]
@@ -107,64 +122,53 @@ cmd [[command! SplitNewline call feedkeys("i<CR><Esc>", "t")]]
 cmd [[command! NSubstitute call feedkeys(":s/", "t")]]
 cmd [[command! YankLine call feedkeys('^y$', "t")]]
 cmd [[command! YankLineToClip call feedkeys('^v$h"*y', "t")]]
-nleader("xij", [[:InsertBelow<CR>]])
-nleader("xik", [[:InsertAbove<CR>]])
-nleader("xm", [[:RenderMarkdown toggle<CR>]])
-nleader("xn", [[:SplitNewline<CR>]])
-nleader("xs", [[:NSubstitute<CR>]])
-vleader("xs", [[:NSubstitute<CR>]])
-nleader("xy", [[:YankLine<CR>]])
-nleader("xY", [[:YankLineToClip<CR>]])
+nleader("xij", [[:InsertBelow<CR>]], "Insert line below")
+nleader("xik", [[:InsertAbove<CR>]], "Insert line above")
+nleader("xm", [[:RenderMarkdown toggle<CR>]], "Toggle markdown preview")
+nleader("xn", [[:SplitNewline<CR>]], "Split at cursor")
+nleader("xs", [[:NSubstitute<CR>]], "Substitute")
+vleader("xs", [[:NSubstitute<CR>]], "Substitute")
+nleader("xy", [[:YankLine<CR>]], "Yank line")
+nleader("xY", [[:YankLineToClip<CR>]], "Yank line to clipboard")
 
 -- Text/Align
 cmd [[command! AlignColon call feedkeys(":Tab /:\zs/l0l1<CR>", "t")]]
 cmd [[command! AlignEqual call feedkeys(":Tab /=/<CR>", "t")]]
 cmd [[command! AlignComma call feedkeys(":Tab /,\zs<CR>", "t")]]
-vleader("xa:", [[:AlignColon<CR>]])
-vleader("xa=", [[:AlignEqual<CR>]])
-vleader("xa,", [[:AlignComma<CR>]])
+vleader("xa:", [[:AlignColon<CR>]], "Align by colon")
+vleader("xa=", [[:AlignEqual<CR>]], "Align by equals")
+vleader("xa,", [[:AlignComma<CR>]], "Align by comma")
 
 -- Window
-nleader("wc", [[:close<CR>]])
-nleader("wh", [[:wincmd H<CR>]])
-nleader("wj", [[:wincmd J<CR>]])
-nleader("wk", [[:wincmd K<CR>]])
-nleader("wl", [[:wincmd L<CR>]])
-nleader("wo", [[:only<CR>]])
-nleader("wx", [[:split<CR>]])
-nleader("wv", [[:vsplit<CR>]])
-nleader("ww", [[:Windows<CR>]])
-nleader("w,", [[:tabprevious<CR>]])
-nleader("w.", [[:tabnext<CR>]])
+nleader("wc", [[:close<CR>]], "Close window")
+nleader("wh", [[:wincmd H<CR>]], "Move window left")
+nleader("wj", [[:wincmd J<CR>]], "Move window down")
+nleader("wk", [[:wincmd K<CR>]], "Move window up")
+nleader("wl", [[:wincmd L<CR>]], "Move window right")
+nleader("wo", [[:only<CR>]], "Close other windows")
+nleader("wx", [[:split<CR>]], "Horizontal split")
+nleader("wv", [[:vsplit<CR>]], "Vertical split")
+nleader("ww", [[:Windows<CR>]], "List windows")
+nleader("w,", [[:tabprevious<CR>]], "Previous tab")
+nleader("w.", [[:tabnext<CR>]], "Next tab")
 
-nleader("wtc", [[:tabclose<CR>]])
-nleader("wth", [[:tabmove -1<CR>]])
-nleader("wtj", [[:tabmove $<CR>]])
-nleader("wtk", [[:tabmove 0<CR>]])
-nleader("wtl", [[:tabmove +1<CR>]])
-nleader("wto", [[:tabonly<CR>]])
-nleader("wtt", [[:tabnew<CR>]])
-nleader("wt,", [[:tabfirst<CR>]])
-nleader("wt.", [[:tablast<CR>]])
+nleader("wtc", [[:tabclose<CR>]], "Close tab")
+nleader("wth", [[:tabmove -1<CR>]], "Move tab left")
+nleader("wtj", [[:tabmove $<CR>]], "Move tab to last")
+nleader("wtk", [[:tabmove 0<CR>]], "Move tab to first")
+nleader("wtl", [[:tabmove +1<CR>]], "Move tab right")
+nleader("wto", [[:tabonly<CR>]], "Close other tabs")
+nleader("wtt", [[:tabnew<CR>]], "New tab")
+nleader("wt,", [[:tabfirst<CR>]], "First tab")
+nleader("wt.", [[:tablast<CR>]], "Last tab")
 
--- Flutter
-nleader("vr", [[:FlutterRun<CR>]])
-nleader("vR", [[:FlutterRestart<CR>]])
-nleader("vh", [[:FlutterReload<CR>]])
-nleader("vd", [[:FlutterDevices<CR>]])
-nleader("vo", [[:FlutterOutlineToggle<CR>]])
-
--- Claude Code
-nleader("cc", [[:ClaudeCode<CR>]])
-nleader("cf", [[:ClaudeCodeFocus<CR>]])
-nleader("ca", [[:ClaudeCodeAdd %<CR>]])
-nleader("cda", [[:ClaudeCodeDiffAccept<CR>]])
-nleader("cdd", [[:ClaudeCodeDiffDeny<CR>]])
-vleader("cs", [[:ClaudeCodeSend<CR>]])
+-- QuickFix
+nleader("Fc", [[:cclose<CR>]], "Close quickfix")
+nleader("Fo", [[:copen<CR>]], "Open quickfix")
 
 -- Quit
-nleader("qq", [[:qa<CR>]])
-nleader("qQ", [[:qa!<CR>]])
+nleader("qq", [[:qa<CR>]], "Quit all")
+nleader("qQ", [[:qa!<CR>]], "Force quit all")
 
 -- which-key group labels
 local wk = require("which-key")
@@ -188,7 +192,6 @@ wk.add({
   { "<leader>x",  group = "Text" },
   { "<leader>xi", group = "Insert" },
   { "<leader>xa", group = "Align" },
-  { "<leader>ef", desc = "Toggle format on save" },
   { "<leader>v",  group = "Flutter" },
   { "<leader>z",  group = "Terminal" },
   { "<leader>c",  group = "Claude" },
--- config/nvim/lua/config/lsp_shared.lua
+++ config/nvim/lua/config/lsp_shared.lua
@@ -15,27 +15,28 @@ if ok then
 end
 
 M.on_attach = function(_, bufnr)
-  local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local opts = { noremap = true, silent = true }
+  local function map(mode, lhs, rhs, desc)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
+  end
 
-  map("n", "<Leader>ld", [[<cmd>lua vim.lsp.buf.definition()<CR>]], opts)
-  map("n", "<Leader>lD", [[<cmd>lua vim.lsp.buf.implementation()<CR>]], opts)
-  map("n", "<Leader>lf", [[<cmd>lua require("conform").format({ async = true, lsp_fallback = true })<CR>]], opts)
-  map("n", "<Leader>lh", [[<cmd>lua vim.lsp.buf.hover()<CR>]], opts)
-  map("n", "<Leader>lH", [[<cmd>lua vim.lsp.buf.signature_help()<CR>]], opts)
-  map("n", "<Leader>la", [[<cmd>lua vim.lsp.buf.code_action()<CR>]], opts)
-  map("v", "<Leader>la", [[<cmd>lua vim.lsp.buf.code_action()<CR>]], opts)
-  map("n", "<Leader>lr", [[<cmd>lua vim.lsp.buf.rename()<CR>]], opts)
-  map("n", "<Leader>lR", [[<cmd>lua vim.lsp.buf.references()<CR>]], opts)
-  map("n", "<Leader>ls", [[<cmd>Telescope lsp_document_symbols<CR>]], opts)
-  map("n", "<Leader>lS", [[<cmd>Telescope lsp_workspace_symbols<CR>]], opts)
-  map("n", "<Leader>li", [[<cmd>lua vim.lsp.buf.incoming_calls()<CR>]], opts)
-  map("n", "<Leader>lw", [[<cmd>Telescope diagnostics<CR>]], opts)
-  map("n", "<Leader>lt", [[<cmd>lua vim.lsp.buf.type_definition()<CR>]], opts)
-  map("n", "<Leader>lL", [[<cmd>LspLog<CR>]], opts)
-  map("n", "<Leader>dd", [[<cmd>lua vim.diagnostic.open_float()<CR>]], opts)
-  map("n", "<Leader>dn", [[<cmd>lua vim.diagnostic.goto_next()<CR>]], opts)
-  map("n", "<Leader>dp", [[<cmd>lua vim.diagnostic.goto_prev()<CR>]], opts)
+  map("n", "<Leader>ld", [[<cmd>lua vim.lsp.buf.definition()<CR>]],                                        "Go to definition")
+  map("n", "<Leader>lD", [[<cmd>lua vim.lsp.buf.implementation()<CR>]],                                    "Go to implementation")
+  map("n", "<Leader>lf", [[<cmd>lua require("conform").format({ async = true, lsp_fallback = true })<CR>]], "Format")
+  map("n", "<Leader>lh", [[<cmd>lua vim.lsp.buf.hover()<CR>]],                                             "Hover docs")
+  map("n", "<Leader>lH", [[<cmd>lua vim.lsp.buf.signature_help()<CR>]],                                    "Signature help")
+  map("n", "<Leader>la", [[<cmd>lua vim.lsp.buf.code_action()<CR>]],                                       "Code action")
+  map("v", "<Leader>la", [[<cmd>lua vim.lsp.buf.code_action()<CR>]],                                       "Code action")
+  map("n", "<Leader>lr", [[<cmd>lua vim.lsp.buf.rename()<CR>]],                                            "Rename")
+  map("n", "<Leader>lR", [[<cmd>lua vim.lsp.buf.references()<CR>]],                                        "References")
+  map("n", "<Leader>ls", [[<cmd>Telescope lsp_document_symbols<CR>]],                                      "Document symbols")
+  map("n", "<Leader>lS", [[<cmd>Telescope lsp_workspace_symbols<CR>]],                                     "Workspace symbols")
+  map("n", "<Leader>li", [[<cmd>lua vim.lsp.buf.incoming_calls()<CR>]],                                    "Incoming calls")
+  map("n", "<Leader>lw", [[<cmd>Telescope diagnostics<CR>]],                                               "Workspace diagnostics")
+  map("n", "<Leader>lt", [[<cmd>lua vim.lsp.buf.type_definition()<CR>]],                                   "Type definition")
+  map("n", "<Leader>lL", [[<cmd>LspLog<CR>]],                                                              "LSP log")
+  map("n", "<Leader>dd", [[<cmd>lua vim.diagnostic.open_float()<CR>]],                                     "Show diagnostic")
+  map("n", "<Leader>dn", [[<cmd>lua vim.diagnostic.goto_next()<CR>]],                                      "Next diagnostic")
+  map("n", "<Leader>dp", [[<cmd>lua vim.diagnostic.goto_prev()<CR>]],                                      "Previous diagnostic")
 end
 
 return M
