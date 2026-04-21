6d1f714efbdaf5d61085cab376967b0f70d6cfb8
Brasco Cooke <ron@astonspark.com>
2026-04-20 21:12:06 -0400

    Add Lazygit as alt git client in Neovim

--- config/nvim/lua/config/plugins.lua
+++ config/nvim/lua/config/plugins.lua
@@ -35,6 +35,10 @@ require("lazy").setup({
     "lewis6991/gitsigns.nvim",
     config = function() require("gitsigns").setup() end,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
   { "folke/tokyonight.nvim" },
   {
     "folke/trouble.nvim",
